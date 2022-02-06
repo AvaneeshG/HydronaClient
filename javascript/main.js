98d133d1d3ea1be9f889f3770cce29b8b9cfdaba
Avaneesh Naik <avaneeshnaik2663@gmail.com>
2022-02-07 00:34:42 +0530

    chupapi

--- javascript/main.js
+++ javascript/main.js
@@ -2,6 +2,8 @@ var url = new URL(window.location.href)
 var down = document.getElementById('doxx');
 
 function query() {
-    url.searchParams.append('query', 'ddos');
-    window.history.replaceState(null, document.title, url) // switch to url kek
+    //url.searchParams.append('query', 'ddos');
+    url.searchParams.append(document.getElementById('myText'));
+    down.innerHTML = url;
+    window.history.pushState(null, document.title, url) // switch to url kek
 }
