304041c6bff512157976155accacd0c93ee9fcf1
Avaneesh Naik <avaneeshnaik2663@gmail.com>
2022-02-06 23:30:43 +0530

    doxx test 2

--- javascript/main.js
+++ javascript/main.js
@@ -2,4 +2,5 @@ function ready() {
     //injecty vejecty fixed no longer leaked?1!
     let query = new URL(window.location).searchParams.get('query');
     document.getElementById('myText').value = query;
+    document.getElementById('doxx').innerHTML = query;
 }
